7566c8d9ee56380e9469f5430aca7ae5c82b1468
nguyenan123hp <nguyenanhp04@gmail.com>
2024-10-21 12:26:22 +0700

    commit test

---
+++ src/main/java/vn/viettuts/qlsv/entity/Teacher.java
@@ -0,0 +1,50 @@
+package vn.viettuts.qlsv.entity;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "teacher")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Teacher implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String fullName;
+    private String specialty;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id, String fullName, String specialty) {
+        this.id = id;
+        this.fullName = fullName;
+        this.specialty = specialty;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+}
